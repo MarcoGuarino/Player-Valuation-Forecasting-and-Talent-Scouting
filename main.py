72768e5f70d262114a94739bdb9b6e7848314ec2
MarcoGuarino <guarino.1895383@studenti.uniroma1.it>
2023-05-09 14:27:59 +0200

    new features to the final dataframe

--- main.py
+++ main.py
@@ -1,25 +1,28 @@
 import pyspark
 from pyspark import SparkConf
+from pyspark import SparkContext
 from pyspark.sql import SparkSession
 import pre_processing
 
 def create_spark_session():
     # Create the session
-    conf = SparkConf(). \
-        set('spark.ui.port', "4050"). \
-        set('spark.executor.memory', '10G'). \
-        set('spark.driver.memory', '45G'). \
-        set('spark.driver.maxResultSize', '30G'). \
-        setAppName("PySparkTutorial"). \
-        set('spark.executor.cores', "8"). \
-        setMaster("local[*]")
+    if 'sc' in locals() or 'sc' in globals():
+        print("Spark context already created") 
+        sc.stop()
+    conf = SparkConf().\
+                    set('spark.ui.port', "4050").\
+                    set('spark.executor.memory', '4G').\
+                    set('spark.driver.memory', '45G').\
+                    set('spark.driver.maxResultSize', '10G').\
+                    setAppName("PySparkTutorial").\
+                    setMaster("local[*]")
 
-    sc = pyspark.SparkContext.getOrCreate(conf=conf)
-    #sc.stop()
+    # Create the context
+    sc = pyspark.SparkContext(conf=conf)
     spark = SparkSession.builder.getOrCreate()
 
-
 def run_preprocessing():
+    print("Running preprocessing...")
     final_table = pre_processing.create_final_table()
 
 if __name__ == "__main__":
